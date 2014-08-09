093b3e76e9ccb6ed8d32af7dfeba7e042fa329a2
Peter Dunlop <peterdunlop@Peters-MacBook-Pro.local>
2014-08-08 21:52:02 -0700

    Initial Commit

---
+++ SYNC1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SYNC1
+//
+//  Created by Peter Dunlop on 8/8/14.
+//  Copyright (c) 2014 tpetdu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SYNC1Tests/SYNC1Tests.m
@@ -0,0 +1,34 @@
+//
+//  SYNC1Tests.m
+//  SYNC1Tests
+//
+//  Created by Peter Dunlop on 8/8/14.
+//  Copyright (c) 2014 tpetdu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SYNC1Tests : XCTestCase
+
+@end
+
+@implementation SYNC1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
